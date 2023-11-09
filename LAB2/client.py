b824d6f6c0832203bfeeda1937a0e5c85083caf8
Adel Pazoki Toroudi <apazokit@uwaterloo.ca>
2023-11-09 16:50:54 -0500

    code clean up

--- LAB2/client.py
+++ LAB2/client.py
@@ -7,39 +7,28 @@ def create_dns_query(domain):
     # Generate a random ID for the query
     transaction_id = random.randint(0, 255)
 
-    # Initial flags set to 0
     flags = 0
 
     # Set AA (bit 10)
-    # For an authoritative answer
     flags |= (1 << 10)
    
-    # Question Count: 1
     qdcount = 1
     
-    # Answer Count, Name Server Count, Additional Records Count: 0
     ancount = nscount = arcount = 0
     
-    # Create the DNS Header
     header = struct.pack('>HHHHHH', transaction_id, flags, qdcount, ancount, nscount, arcount)
     
-    # Create the DNS Question
-    # Split the domain by '.', prepend length of each part
     qname = b''.join(len(part).to_bytes(1, 'big') + part.encode() for part in domain.split('.'))
     
-    # Add the final null byte to the domain name
+    # Final null byte to the domain name
     qname += b'\x00'
     
-    # Type A (host address)
     qtype = 1
     
-    # Class IN (Internet)
     qclass = 1
     
-    # Pack the question fields
     question = struct.pack(f'>{len(qname)}sHH', qname, qtype, qclass)
     
-    # Return the query
     return header + question
 
 def print_hex(data):
@@ -55,40 +44,32 @@ def main():
     
         query = create_dns_query(domain)
     
-        # The following lines would be used to send the query to the server
-        # For now, we will just print the query
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        server_address = ('127.0.0.1', 10500) # replace 53 with your chosen port
+        server_address = ('127.0.0.1', 10500)
         sock.sendto(query, server_address)
 
-        # Here you would receive the response from the server and process it
-        response, _ = sock.recvfrom(512) # 512 is a safe value for the buffer size
+        response, _ = sock.recvfrom(512) 
 
         header_length = 12
-        question_length = response[header_length:].index(0) + 5  # +5 for QTYPE and QCLASS
+        question_length = response[header_length:].index(0) + 5
         answer_section_start = header_length + question_length
 
-        # There are two answers in this response, so we'll loop twice
+        # Range is 2 since google has 2 ip addresses
         answers = []
         for i in range(2):  # two answers
-            # Each answer is 16 bytes long, but we'll extract RDATA separately due to its variable length
             answer_start = answer_section_start + i * 16
             answer = response[answer_start:answer_start + 12]
             if (answer == b''):
                 continue
             _, type_, class_, ttl, rdlength = struct.unpack('>HHHLH', answer)
             
-            # Calculate the start of the RDATA field and extract it
             rdata_start = answer_start + 12
             rdata = response[rdata_start:rdata_start + rdlength]
 
-            # Convert RDATA to an IP address
             ip_address = socket.inet_ntoa(rdata)
     
-            # Append to the list of answers
             answers.append((type_, class_, ttl, rdlength, ip_address))
 
-        # Print the results
         for answer in answers:
             type_, class_, ttl, rdlength, ip_address = answer
             type_str = 'A' if type_ == 1 else 'Unknown'
--- LAB2/server.py
+++ LAB2/server.py
@@ -1,7 +1,6 @@
 import socket
 import struct
 
-# Predefined DNS records as per the table provided
 DNS_RECORDS = {
     'google.com': [('192.165.1.1', 260), ('192.165.1.10', 260)],
     'youtube.com': [('192.165.1.10', 160)],
@@ -10,15 +9,13 @@ DNS_RECORDS = {
     'amazon.ca': [('192.165.1.5', 160)]
 }
 
-# Server's IP and Port
 SERVER_IP = '127.0.0.1'
 SERVER_PORT = 10500
 
+# parse the query received from client
 def parse_query(data):
-    # Unpack the query header
     transaction_id, flags, qdcount = struct.unpack('>HHH', data[:6])
     
-    # Read the domain name from the question section
     pointer = 12
     domain_parts = []
     while True:
@@ -32,38 +29,33 @@ def parse_query(data):
 
     return transaction_id, domain_name
 
+#make the response to be sent back to the client
 def create_response(transaction_id, domain_name):
-    # Check if the domain is in the predefined table
     if domain_name in DNS_RECORDS:
-        # Start building the response
-        flags = 0x8400  # Standard DNS response and no error
+        flags = 0x8400  
         qdcount = 1
         ancount = len(DNS_RECORDS[domain_name])
         nscount = 0
         arcount = 0
         header = struct.pack('>HHHHHH', transaction_id, flags, qdcount, ancount, nscount, arcount)
 
-        # Build the question section
         question = b''
         for part in domain_name.split('.'):
             question += struct.pack('B', len(part)) + part.encode()
-        question += struct.pack('BHB', 0, 1, 1)  # Name ends with 0, QTYPE=A, QCLASS=IN
-
-        # Build the answer section
+        question += struct.pack('BHB', 0, 1, 1) 
+        
         answer = b''
         for ip, ttl in DNS_RECORDS[domain_name]:
             answer += struct.pack('>HHHLH4s', 0xC00C, 1, 1, ttl, 4, socket.inet_aton(ip))
 
         return header + question + answer
     else:
-        # Domain not found, send an empty response
         return b''
 
 def print_hex(data):
     print(" ".join(f"{b:02x}" for b in data))
 
 def run_server():
-    # Open the server socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((SERVER_IP, SERVER_PORT))
 
